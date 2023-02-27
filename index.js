f978b5114d9d441231193c97918f808af0fabb21
Nguyen Tang <kyogen@Nguyens-MBP.lan>
2023-02-27 12:02:41 +1030

    Scroll reveal did a thing

--- index.js
+++ index.js
@@ -1,4 +1,29 @@
-const sr = ScrollReveal({ reset: true});
+// Add class text-reveal to all the child within text-content-container class
+const textContainer = document.querySelectorAll('.text-content-container');
 
-sr.reveal('.text-content-container', { interval: 500 });
-console.log("ScrollReveal");
+function addTextReveal(){
+    for (let i = 0; i < textContainer.length; i++){
+        const current = textContainer[i];
+        for (const el of current.children){
+            el.classList.add('text-reveal');
+        }
+    }
+    console.log("Add text-reveal class to all elements under text-content-container");
+};
+
+// Scroll Reveal
+function scrollRevealStart(){
+    const sr = ScrollReveal();
+
+    sr.reveal('.text-reveal');
+    console.log("ScrollReveal");
+}
+
+// On load
+window.onload = () => {
+    console.log("Loading");
+    $(".loader").fadeOut("slow");
+    addTextReveal();
+    $('.text-reveal').addClass('load-hidden');
+    scrollRevealStart();
+};
