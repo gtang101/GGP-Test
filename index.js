c5b5ea582bdfea89cd711dc255048430211126da
Nguyen Tang <kyogen@Nguyens-MBP.lan>
2023-03-09 12:31:25 +1030

    swap illus, text, spacing

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Add class text-reveal to all the child within text-content-container class
-const textContainer = document.querySelectorAll('.text-content-container');
-
+const textContainer = document.querySelectorAll(".text-content-container, .special-case-text");
+		
 function addTextReveal(){
     for (let i = 0; i < textContainer.length; i++){
         const current = textContainer[i];
@@ -13,9 +13,12 @@ function addTextReveal(){
 
 // Scroll Reveal
 function scrollRevealStart(){
-    const sr = ScrollReveal( {reset: true} );
+    const sr = ScrollReveal( {viewFactor: 0.6, delay: 100} );
 
     sr.reveal('.text-reveal');
+    sr.reveal('.sequence', { interval: 500, viewFactor: 1});
+    sr.reveal('.sequence-a', { interval: 500, viewFactor: 0});
+    sr.reveal('.img-reveal', {viewFactor: 0.3})
     console.log("ScrollReveal");
 }
 
@@ -24,7 +27,9 @@ window.onload = () => {
     console.log("Loading");
     $(".loader").fadeOut("slow");
     addTextReveal();
-    $('.text-reveal').addClass('load-hidden');
+    $(".text-reveal, .sequence, .sequence-a, .img-reveal").addClass('load-hidden');
     scrollRevealStart();
     console.log("Loaded");
 };
+
+// Track where on the page
